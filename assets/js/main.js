ff268c4fe5bdb20a662415fa5b3de13630ac3820
nozomi <89883172+nozomikubota@users.noreply.github.com>
2021-09-16 13:33:05 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,7 +112,7 @@ app.component('answer-input', {
       okMessage: '正解おめでとう(^○^)',
       ngMessage: '残念！答えが違うみたい(´· ·`)',
       message: '',
-      inputAnswer: 'ひらがなもしくは半角英字',
+      inputAnswer: '<span style="color: #dd99ff">ひらがなもしくは半角英字</span>',
     }
   },
   template: `
